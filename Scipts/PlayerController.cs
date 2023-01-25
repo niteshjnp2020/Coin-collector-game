496f29bce2200808df46669d477610a81c4e0f3e
Nitesh kumar chaurasiya <123541512+niteshjnp2020@users.noreply.github.com>
2023-01-25 14:06:21 +0530

    Add files via upload

---
+++ Scipts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public TextMeshProUGUI countText, winText;
+
+    private Rigidbody rb;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+    }
+
+    
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal,0,moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Coin - " + count.ToString();
+            if (count >= 10)
+                winText.gameObject.SetActive(true);
+        }
+    }
+}
